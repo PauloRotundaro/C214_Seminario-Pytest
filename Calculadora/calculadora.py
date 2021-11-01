a2e4d50e6d7115a60d8f4f8b23db2a922cb7e790
Paulo Rotundaro <paulo.rotundaro@toodoo.com.br>
2021-11-01 12:18:28 -0300

    Feat: 'Calculadora' file added to project

---
+++ Calculadora/calculadora.py
@@ -0,0 +1,15 @@
+class Calculadora:
+    def soma(x, y):
+        return x + y
+
+    def subtracao(x, y):
+        return x - y
+
+    def multiplicacao(x, y):
+        return x * y
+
+    def divisao(x, y):
+        if y == 0:
+            return 0
+        else:
+            return x / y
