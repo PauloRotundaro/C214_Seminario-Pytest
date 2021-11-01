ae41b44a2ed186c167d02271852a85be67d5eeab
Paulo Rotundaro <paulo.rotundaro@toodoo.com.br>
2021-11-01 13:16:51 -0300

    Improv: Test separation

--- Calculadora/test/test_soma_subtracao.py
+++ Calculadora/test/test_soma_subtracao.py
@@ -4,12 +4,6 @@ import pytest
 def teste_soma():
     assert c.Calculadora.soma(1, 2) == 3
 
-def teste_divisao():
-    assert c.Calculadora.divisao(4, 2) == 2
-
-def teste_multiplicacao():
-    assert c.Calculadora.multiplicacao(12, 2) == 24
-
 def teste_subtracao():
     assert c.Calculadora.subtracao(9, 3) == 6
 
